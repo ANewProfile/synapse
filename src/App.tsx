ed77afaf1fd093a5fd68ca3657f2e74907a51ee3
Theodore Chen <littletheo@Theodores-MacBook-Air.local>
2025-04-19 20:55:07 -0400

    cleaned up routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import TestPomo from "./pages/TestPomo";
+import PomoTimerPage from "./pages/PomoTimerPage";
 import { Routes, Route } from "react-router-dom";
 import "./css/App.css";
-import PomoTimerPage from "./pages/PomoTimerPage";
 
 function App() {
     return (
@@ -14,14 +13,13 @@ function App() {
             <main className="main-content flex-grow-1 d-flex flex-column">
                 <Routes>
                     <Route path="/" element={<Dashboard />} />
+                    <Route path="/pomodoro" element={<PomoTimerPage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route
                         path="/logout"
                         element={<p>LOGOUT NOT IMPLEMENTED</p>}
                     />
-                    <Route path="/testpomo" element={<TestPomo />} />
-                    <Route path="/pomodoro" element={<PomoTimerPage />} />
                 </Routes>
             </main>
         </div>
