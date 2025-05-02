10b3e06297a861b904be4a4edd907aec4e646241
Theodore Chen <littletheo@Theodores-MacBook-Air.local>
2025-05-02 08:08:45 -0400

    fixed dashboard height

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,24 +9,24 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    //yea the url needs to be adjusted on later
-    //this part is to check if already logged in or not
-    axios
-      .get("http://localhost:3000/api/getme")
-      .then((res) => {
-        setUser(res.data);
-      })
-      .catch((error) => {
-        setUser(null);
-        console.log("User Auth frontend error");
-      });
-  }, []);
+    useEffect(() => {
+        //yea the url needs to be adjusted on later
+        //this part is to check if already logged in or not
+        axios
+            .get("http://localhost:3000/api/getme")
+            .then((res) => {
+                setUser(res.data);
+            })
+            .catch((error) => {
+                setUser(null);
+                console.log("User Auth frontend error");
+            });
+    }, []);
 
-  //navigate dont work outside routes huh
-  /** 
+    //navigate dont work outside routes huh
+    /** 
   if (!user) {
     return (
       <>
@@ -34,25 +34,26 @@ function App() {
       </>
     );
   }*/
-  return (
-    <div className="d-flex flex-column" style={{ height: "120vh" }}>
-      <Navbar />{" "}
-      {/**dont use 100vh hardcoded for height here.. u can use full/100% for height */}
-      <main className="main-content flex-grow-1 d-flex flex-column">
-        {/**this part needs to be responsive on phone max col for grid 2  */}
-        <Routes>
-          <Route
-            path="/"
-            element={!user ? <Dashboard /> : <Navigate to="/login" />}
-          />
-          <Route path="/pomodoro" element={<PomoTimerPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/logout" element={<Login />} />
-        </Routes>
-      </main>
-    </div>
-  );
+    return (
+        <div className="d-flex flex-column">
+            <Navbar />{" "}
+            <main className="main-content flex-grow-1 d-flex flex-column">
+                {/**this part needs to be responsive on phone max col for grid 2  */}
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            !user ? <Dashboard /> : <Navigate to="/login" />
+                        }
+                    />
+                    <Route path="/pomodoro" element={<PomoTimerPage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/logout" element={<Login />} />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;
